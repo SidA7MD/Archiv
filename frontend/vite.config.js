64844dfc805a7095494fe7baac0ce50b66d4ecf9
Sidi110 <24110@supnum.mr>
2025-07-15 21:41:16 +0000

    new edits

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/Archiv/' // obligatoire pour GitHub Pages
+  base: '/', // ← très important ici
 })
